acb0ab3ce5ba836c3e75d14cf5f14b296c083fa3
Your Namemdrafiqulhasan110 <mdrafiqulhasan110@gmail.com>
2023-08-21 22:00:22 +0600

    version2

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ let sizeField = document.getElementById("size");
 let colorField = document.getElementById("color");
 
 
-function decoration(e,type){
-    if(areas.classList.contains(type)){
-       areas.classList.remove(type);
-       e.classList.remove('bg-black');
+function decoration(e, type) {
+    if (areas.classList.contains(type)) {
+        areas.classList.remove(type);
+        e.classList.remove('bg-black');
     }
-    else{
+    else {
         areas.classList.add(type);
         e.classList.add('bg-black');
     }
@@ -17,22 +17,23 @@ function decoration(e,type){
 }
 
 
-function align(e,type){
-    areas.style.textAlign=type;
+function align(e, type) {
+    areas.style.textAlign = type;
     let childs = e.parentNode.childNodes;
-    
-    for (let i =1; i<childs.length; i+=2){
+
+    for (let i = 1; i < childs.length; i += 2) {
         console.log(childs[i]);
-childs[i].style.backgroundColor ='transparent'}
-e.style.backgroundColor ='black'
+        childs[i].style.backgroundColor = 'transparent'
+    }
+    e.style.backgroundColor = 'black'
 
 }
 
-sizeField.addEventListener('change', function(){
+sizeField.addEventListener('change', function () {
     console.log(sizeField.value);
-    areas.style.fontSize=sizeField.value+'px';
+    areas.style.fontSize = sizeField.value + 'px';
 })
 
-colorField.addEventListener('change', function(){
-    areas.style.color=colorField.value;
+colorField.addEventListener('change', function () {
+    areas.style.color = colorField.value;
 })
